51ec1c58db28b33379ce2f0b2b57a24132a62c95
macbookpro <mahsinsaifullah@gmail.com>
2024-01-14 13:53:15 +0000

    feat: add form ui

--- src/components/todo/Todo.tsx
+++ src/components/todo/Todo.tsx
@@ -1,8 +1,10 @@
+import { Title } from '../shared';
 import { Form } from './Form';
 
 export const Todo = () => {
   return (
-    <section>
+    <section className="flex flex-col h-[100%] items-center pt-[72px]">
+      <Title />
       <Form />
     </section>
   );

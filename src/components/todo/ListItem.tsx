e11728498d5ec40b8833feb13d6357d766613417
MahsinSaifullah <mahsinsaifullah@gmail.com>
2024-01-18 15:01:21 +0000

    feat: add edit and trash icon for list item

--- src/components/todo/ListItem.tsx
+++ src/components/todo/ListItem.tsx
@@ -1,7 +1,16 @@
+import { PencilIcon, TrashIcon } from "@heroicons/react/24/solid";
+
 export const ListItem = () => {
   return (
-    <li className="min-w-[400px] w-[50%] p-3 rounded-lg bg-slate-500 text-white">
-      ListItem
+    <li className="min-w-[400px] w-[50%] p-3 rounded-lg bg-slate-500 text-white flex items-center justify-between">
+      <div className="flex items-center gap-2">
+        <input type="checkbox" value="" className="w-4 h-4 cursor-pointer" />
+        <h3>List item</h3>
+      </div>
+      <div className="flex items-center gap-4">
+        <PencilIcon className="h-5 w-5 text-white cursor-pointer" />
+        <TrashIcon className="h-5 w-5 text-red-300 cursor-pointer" />
+      </div>
     </li>
   );
 };

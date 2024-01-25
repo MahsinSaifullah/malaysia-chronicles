b8e136a26299ea616b1a6f39a6dae84715b9e7dc
MahsinSaifullah <mahsinsaifullah@gmail.com>
2024-01-25 13:33:21 +0000

    fix: show clearAll logic

--- src/components/todo/Todo.tsx
+++ src/components/todo/Todo.tsx
@@ -1,12 +1,36 @@
 import * as React from "react";
 import * as ReactRouter from "react-router-dom";
 import * as CustomHook from "../../hooks";
-import { ITodoType } from "../../types";
+import { ITodo, ITodoType } from "../../types";
 import { Title } from "../shared";
 import { Form } from "./Form";
 import { GoBack } from "./GoBack";
 import { List } from "./List";
 import { ClearAll } from "./ClearAll";
+
+const showClearAll = (
+  typeOfTodo: string | undefined,
+  takeTodo: ITodo[],
+  bringTodo: ITodo[]
+) => {
+  if (!typeOfTodo) {
+    return false;
+  }
+
+  if (!takeTodo.length && !bringTodo.length) {
+    return false;
+  }
+
+  if (typeOfTodo === "take" && !takeTodo.length) {
+    return false;
+  }
+
+  if (typeOfTodo === "bring" && !bringTodo.length) {
+    return false;
+  }
+
+  return true;
+};
 
 export const Todo = () => {
   const navigate = ReactRouter.useNavigate();
@@ -21,12 +45,15 @@ export const Todo = () => {
     setTodoType(typeOfTodo as ITodoType);
   }, [typeOfTodo]); // eslint-disable-line react-hooks/exhaustive-deps
 
+  const takeTodos = todos.filter((todo) => todo.type === "take");
+  const bringTodos = todos.filter((todo) => todo.type === "bring");
+
   return (
     <section className="flex flex-col h-[100%] items-center pt-[72px] overflow-scroll pb-10 relative">
       <GoBack />
       <Title />
       <Form />
-      {!!todos.length && <ClearAll />}
+      {showClearAll(typeOfTodo, takeTodos, bringTodos) && <ClearAll />}
       <List />
     </section>
   );

5a42c7d7452c434b58e8b910d55a659febd21d89
macbookpro <mahsinsaifullah@gmail.com>
2024-02-19 23:03:18 +0000

    refactor: render landing page button based on remote todo type

--- src/components/landing/Landing.tsx
+++ src/components/landing/Landing.tsx
@@ -5,6 +5,7 @@ import { Title } from '../shared';
 
 export const Landing = () => {
   const { setTodoType } = CustomHook.useTodo();
+  const { allTodoTypes } = CustomHook.useTodoType();
 
   React.useEffect(() => {
     setTodoType(null);
@@ -18,19 +19,18 @@ export const Landing = () => {
           Choose your path:
         </h3>
       </div>
-      <div className="flex justify-center w-1/2 gap-x-8">
-        <Link
-          to="/todo/take"
-          className="text-sm md:text-lg w-1/2 max-w-[300px] min-w-[150px] bg-slate-500 hover:bg-slate-600 text-white  py-2 px-4 rounded text-center"
-        >
-          To Take
-        </Link>
-        <Link
-          to="/todo/bring"
-          className="text-sm md:text-lg w-1/2 max-w-[300px] min-w-[150px] bg-slate-500 hover:bg-slate-600 text-white  py-2 px-4 rounded text-center"
-        >
-          To Bring
-        </Link>
+      <div className="grid grid-cols-2 justify-center gap-8">
+        {allTodoTypes.map((todoType) => {
+          return (
+            <Link
+              to={`/todo/${todoType.type}`}
+              key={todoType.id}
+              className="text-sm md:text-lg w-1/2 min-w-[150px] md:min-w-[200px] bg-slate-500 hover:bg-slate-600 text-white py-2 px-4 rounded text-center"
+            >
+              To {todoType.type}
+            </Link>
+          );
+        })}
       </div>
     </section>
   );
